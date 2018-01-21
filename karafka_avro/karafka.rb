474058983de0e76a418713d27645deabd1947270
Bernard Potocki <bernard.potocki@imanel.org>
2018-01-21 16:52:52 +0100

    Copy Karafka json to avro

---
+++ karafka_avro/karafka.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+ENV['RACK_ENV'] ||= 'development'
+ENV['KARAFKA_ENV'] ||= ENV['RACK_ENV']
+Bundler.require(:default, ENV['KARAFKA_ENV'])
+Karafka::Loader.load(Karafka::App.root)
+Karafka.logger.level = Logger::WARN
+
+class KarafkaApp < Karafka::App
+  setup do |config|
+    config.kafka.seed_brokers = %w[kafka://127.0.0.1:9092]
+    config.client_id = 'karafka_avro'
+    config.backend = :inline
+    config.batch_fetching = true
+  end
+
+  consumer_groups.draw do
+    topic :kafka_bench_avro do
+      controller ApplicationController
+    end
+  end
+end
+
+KarafkaApp.boot!
