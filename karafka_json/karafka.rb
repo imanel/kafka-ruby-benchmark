cc050dbde6f4334f0165d1b755b855e034b246ad
Bernard Potocki <bernard.potocki@imanel.org>
2018-01-22 10:28:48 +0100

    Use JSON Gem natively instead of MultiJson as it gives ~8% speed improvement

--- karafka_json/karafka.rb
+++ karafka_json/karafka.rb
@@ -6,12 +6,23 @@ Bundler.require(:default, ENV['KARAFKA_ENV'])
 Karafka::Loader.load(Karafka::App.root)
 Karafka.logger.level = Logger::WARN
 
+require 'json'
+
+class JsonGemParser
+  def self.parse(message)
+    ::JSON.parse(message)
+  rescue ::JSON::JSONError => e
+    raise ::Karafka::Errors::ParserError, e
+  end
+end
+
 class KarafkaApp < Karafka::App
   setup do |config|
     config.kafka.seed_brokers = %w[kafka://127.0.0.1:9092]
     config.client_id = 'karafka_json'
     config.backend = :inline
     config.batch_fetching = true
+    config.parser = JsonGemParser
   end
 
   consumer_groups.draw do
